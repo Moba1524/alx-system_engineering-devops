fa737b824b16174c9653d41397773138d7c16e07
root <mothafr2016@gmail.com>
2023-08-08 13:48:05 -0700

    Task 100

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,47 +1,88 @@
 #!/usr/bin/python3
-"""Module for task 3"""
+"""A module containing functions for Task 3.
+"""
+import requests
 
 
-def count_words(subreddit, word_list, word_count={}, after=None):
-    """Queries the Reddit API and returns the count of words in
-    word_list in the titles of all the hot posts
-    of the subreddit"""
-    import requests
+def hist_sort(hist={}):
+    '''Sorts and prints the given histogram.
+    '''
+    hist = list(filter(lambda tup: tup[1], hist))
+    hist_dict = {}
+    for item in hist:
+        if item[0] in hist_dict:
+            hist_dict[item[0]] += item[1]
+        else:
+            hist_dict[item[0]] = item[1]
+    hist = list(hist_dict.items())
+    hist.sort(
+        key=lambda tup: tup[0],
+        reverse=False
+    )
+    hist.sort(
+        key=lambda tup: tup[1],
+        reverse=True
+    )
+    printable_str = '\n'.join(list(map(
+        lambda tup: '{}: {}'.format(tup[0], tup[1]),
+        hist
+    )))
+    if printable_str:
+        print(printable_str)
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json"
-                            .format(subreddit),
-                            params={"after": after},
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code != 200:
-        return None
 
-    info = sub_info.json()
-
-    hot_l = [child.get("data").get("title")
-             for child in info
-             .get("data")
-             .get("children")]
-    if not hot_l:
-        return None
-
-    word_list = list(dict.fromkeys(word_list))
-
-    if word_count == {}:
-        word_count = {word: 0 for word in word_list}
-
-    for title in hot_l:
-        split_words = title.split(' ')
-        for word in word_list:
-            for s_word in split_words:
-                if s_word.lower() == word.lower():
-                    word_count[word] += 1
-
-    if not info.get("data").get("after"):
-        sorted_counts = sorted(word_count.items(), key=lambda kv: kv[0])
-        sorted_counts = sorted(word_count.items(),
-                               key=lambda kv: kv[1], reverse=True)
-        [print('{}: {}'.format(k, v)) for k, v in sorted_counts if v != 0]
+def count_words(subreddit, word_list, hist=[], count=0, after=''):
+    '''
+    Counts the number of times each word in a given wordlist
+    occurs in a given subreddit.
+    '''
+    headers = {
+        'Accept': 'application/json',
+        'User-Agent': ' '.join([
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+            'AppleWebKit/537.36 (KHTML, like Gecko)',
+            'Chrome/97.0.4692.71',
+            'Safari/537.36',
+            'Edg/97.0.1072.62'
+        ])
+    }
+    sort = 'hot'
+    limit = 30
+    result = requests.get(
+        '{}/r/{}/.json?sort={}&limit={}&count={}&after={}'.format(
+            'https://www.reddit.com',
+            subreddit,
+            sort,
+            limit,
+            count,
+            after
+        ),
+        headers=headers,
+        allow_redirects=False
+    )
+    if not hist:
+        word_list = list(map(lambda word: word.lower(), word_list))
+        hist = list(map(lambda word: (word, 0), word_list))
+    if result.status_code == 200:
+        data = result.json()['data']
+        posts = data['children']
+        titles = list(map(lambda post: post['data']['title'], posts))
+        hist = list(map(
+            lambda tup: (tup[0], tup[1] + sum(list(map(
+                lambda txt: txt.lower().split().count(tup[0]),
+                titles
+            )))),
+            hist
+        ))
+        if len(posts) >= limit and data['after']:
+            count_words(
+                subreddit,
+                word_list,
+                hist,
+                count + len(posts),
+                data['after']
+            )
+        else:
+            hist_sort(hist)
     else:
-        return count_words(subreddit, word_list, word_count,
-                           info.get("data").get("after"))
+        return
